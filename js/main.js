f7e22b397266507aaf3bc90222a0a0fa61274e3c
Vitalij <12491862+Vitaljano@users.noreply.github.com>
2022-01-22 09:19:20 +0200

    Add banner section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(function(){
+  $(".banner-section__slider").slick({
+    dots: true,
 
+    prevArrow: `
+      <button class="banner-section__slider-btn banner-section__slider-btnprev">
+        <img src="images/arrow-left.svg" alt="prev">
+      </button>`,
+      nextArrow: `
+      <button class="banner-section__slider-btn banner-section__slider-btnnext">
+        <img src="images/arrow-right.svg" alt="prev">
+      </button>`
+  })
 });
